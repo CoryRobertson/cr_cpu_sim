da68028832742b831eb41b3029ee3713ec971220
coryrobertson <cory.robertson17@gmail.com>
2023-10-12 11:29:21 -0700

    Implemented very basic pixel renderer for the cpu emulator, added TODOs for coming goals

--- cr_cpu_emu/src/main.rs
+++ cr_cpu_emu/src/main.rs
@@ -1,13 +1,104 @@
+use log::error;
+use pixels::{Pixels, SurfaceTexture};
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
+#[allow(unused_imports)]
 use cr_cpu_common::prelude::*;
 
-fn main() {
-    let mut cpu = Cpu::new();
-    cpu.add_to_end(&IAdd(192));
-    cpu.add_to_end(&ISub(64));
-    cpu.add_to_end(&Dump);
-    cpu.add_to_end(&Cmp(ACC, OR));
-    cpu.add_to_end(&JGT(1));
-    cpu.add_to_end(&Dump);
+const SCREEN_WIDTH: u32 = 240;
+const SCREEN_HEIGHT: u32 = 144;
 
-    cpu.execute_until_unknown();
+/// The state of the emulator including its emulation methods and functions
+struct EmuState {
+    // TODO: add a cpu to be emulated, and read its vram, then render it to the screen.
+}
+
+impl EmuState {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn update(&mut self) {
+
+    }
+
+    pub fn draw(&self, frame: &mut [u8]) {
+        for (i,pixel) in frame.chunks_exact_mut(4).enumerate() {
+            let rgba = {
+                if i & 1 == 0 {
+                    [255, 111, 50, 255]
+                } else {
+                    [111, 255, 80, 100]
+                }
+            };
+            pixel.copy_from_slice(&rgba);
+        }
+    }
+
+}
+
+fn log_error<E: std::error::Error + 'static>(fn_name: &str, err: E) {
+    error!("{}: {}", fn_name,err);
+}
+
+fn main() {
+    // let mut cpu = Cpu::new();
+    // cpu.add_to_end(&IAdd(192));
+    // cpu.add_to_end(&ISub(64));
+    // cpu.add_to_end(&Dump);
+    // cpu.add_to_end(&Cmp(ACC, OR));
+    // cpu.add_to_end(&JGT(1));
+    // cpu.add_to_end(&Dump);
+    //
+    // cpu.execute_until_unknown();
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(SCREEN_WIDTH as f64,SCREEN_HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("CR_CPU_SIM")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_tex = SurfaceTexture::new(window_size.width,window_size.height,&window);
+        Pixels::new(SCREEN_WIDTH,SCREEN_HEIGHT,surface_tex).unwrap()
+    };
+
+    let mut state = EmuState::new();
+
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::RedrawRequested(_) = event {
+            // draw pixels into buffer
+            state.draw(pixels.frame_mut());
+            if let Err(err) = pixels.render() {
+                *control_flow = ControlFlow::ExitWithCode(1);
+                log_error("pixels.render", err);
+                return;
+            }
+        }
+        if input.update(&event) {
+            // close window ??
+            if input.key_pressed(VirtualKeyCode::Escape) || input.close_requested() {
+                *control_flow = ControlFlow::ExitWithCode(0);
+                return;
+            }
+            // TODO: pass input to the cpu if/when we need to.
+        }
+
+        state.update();
+        window.request_redraw();
+
+    });
+
+
 }
